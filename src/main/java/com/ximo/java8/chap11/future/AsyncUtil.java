38e5131f4fe73417c2783e3ad83a821dd0ec034a
xikl <1392936858@qq.com>
2019-03-20 23:35:54 +0800

    接着学习 java8异步编程

--- src/main/java/com/ximo/java8/chap11/future/AsyncUtil.java
+++ src/main/java/com/ximo/java8/chap11/future/AsyncUtil.java
@@ -3,6 +3,7 @@ package com.ximo.java8.chap11.future;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.util.Locale;
+import java.util.Random;
 
 /**
  * @author 朱文赵
@@ -14,6 +15,8 @@ public class AsyncUtil {
     /** 格式化 具体可以百度 ##.## 和 00.00 的区别*/
     private static final DecimalFormat FORMATTER = new DecimalFormat("#.##", new DecimalFormatSymbols(Locale.CHINA));
 
+    private static final Random RANDOM = new Random(0);
+
 
     /**
      * 模拟延迟 通常为查询数据库等操作
@@ -21,6 +24,16 @@ public class AsyncUtil {
     public static void delay() {
         try {
             Thread.sleep(1000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /** 随机延迟 */
+    public static void randomDelay() {
+        int delay = 500 + RANDOM.nextInt(2000);
+        try {
+            Thread.sleep(delay);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
